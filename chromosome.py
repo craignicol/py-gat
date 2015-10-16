7842a996aefbc5d1a5c591d099a84e2768fae2d6
craignicol <craig.nicol@gmail.com>
2015-10-16 21:22:22 +0000

    Add random bit field and fitness function

--- chromosome.py
+++ chromosome.py
@@ -1,11 +1,16 @@
 import random
 
+max_length = 8
+
 class chromosome():
-    genome = (0, 0, 0, 0, 0, 0, 0, 0)
+    genome = (0,) * max_length
     
     def __init__(self, genes = ()):
-        self.genome = genes[0:8]
+        l = len(self.genome)
+        self.genome = genes[0:l] + self.genome[len(genes):]
     
     def fitness(self):
         return sum(self.genome) 
         
+def random_chromosome():
+    return chromosome(tuple(random.randint(0, 1) for i in range(max_length)))
--- test_chromosome.py
+++ test_chromosome.py
@@ -1,4 +1,4 @@
-from chromosome import chromosome
+from chromosome import chromosome, random_chromosome
 
 def test_initialiser_correct_length():
     g = (1, 1, 1, 1, 1, 1, 1, 1)
@@ -10,6 +10,21 @@ def test_initialiser_too_long():
     c = chromosome(g)
     assert(c.genome == (1, 1, 1, 1, 1, 1, 1, 1))
 
+def test_initialiser_too_short():
+    g = (1, 1, 1, 1, 1, 1)
+    c = chromosome(g)
+    assert(c.genome == (1, 1, 1, 1, 1, 1, 0, 0))
+
 def test_fitness():
-    c = chromosome()
-    assert(c.fitness() == 0)
+    assert(chromosome((0, 0, 0, 0, 0, 0, 0, 0)).fitness() == 0)
+    assert(chromosome((0, 0, 0, 0, 0, 0, 0, 1)).fitness() == 1)
+    assert(chromosome((0, 0, 0, 0, 0, 0, 1, 1)).fitness() == 2)
+    assert(chromosome((0, 0, 0, 0, 0, 1, 1, 1)).fitness() == 3)
+    assert(chromosome((0, 0, 0, 1, 1, 1, 1, 0)).fitness() == 4)
+    assert(chromosome((1, 1, 1, 1, 1, 0, 0, 0)).fitness() == 5)
+    assert(chromosome((0, 0, 1, 1, 1, 1, 1, 1)).fitness() == 6)
+    assert(chromosome((1, 1, 1, 1, 1, 0, 1, 1)).fitness() == 7)
+    assert(chromosome((1, 1, 1, 1, 1, 1, 1, 1)).fitness() == 8)
+    
+def test_random_chromosome():
+    assert(max([random_chromosome().fitness() for i in range(1000)]) <= 8)
