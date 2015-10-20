d167ce2a47742b75cbb392d59f83c8f60870a6c5
craignicol <craig.nicol@gmail.com>
2015-10-20 19:58:46 +0000

    Add run once

--- population.py
+++ population.py
@@ -1,5 +1,19 @@
+import random
+
 class population():
     pop = []
     
     def __init__(self, size, generator):
         self.pop = [generator() for i in range(size)]
+        
+    def select_two_members(self):
+        return random.sample(self.pop, 2)
+    
+    def run_once(self):
+        "Run one iteration of the genetic algorithm"
+        # Use replacement algorithm
+        new_pop = []
+        for i in range(len(self.pop)):
+            a, b = self.select_two_members()
+            new_pop.append(a + b)
+        self.pop = new_pop
--- test_population.py
+++ test_population.py
@@ -1,4 +1,5 @@
 from population import population
+from chromosome import random_bitarray_chromosome
 from random import randint
 
 import pytest
@@ -6,3 +7,20 @@ import pytest
 def test_generator_creates_enough_members():
     p = population(100, lambda : randint(0, 100))
     assert(len(p.pop) == 100)
+    
+def get_random_population(size):
+    return population(size, random_bitarray_chromosome)
+    
+def test_run_once_doesnt_change_population_size():
+    p = get_random_population(100)
+    p.run_once()
+    assert(len(p.pop) == 100)
+    
+def test_run_once_changes_the_population():
+    p = get_random_population(2)
+    before = p.pop[:] # Copy data, not reference
+    p.run_once()
+    after = p.pop[:]
+    assert(len(after) == len(before) == 2)
+    assert(after[0] != before[0])
+    assert(after[1] != before[1])
